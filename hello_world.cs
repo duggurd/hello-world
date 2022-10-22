3b92f709d7e2c1402790a02365cc88dad87d12c9
Alexander Haugerud <84683233+duggurd@users.noreply.github.com>
2022-10-22 12:24:27 +0200

    Update hello_world.cs

--- hello_world.cs
+++ hello_world.cs
@@ -1,10 +1,8 @@
-﻿namespace hello_world
-{
-    public class hello_world
-    {
-        static void Main(string[] args) 
-        {
-            System.Console.WriteLine("hello, world!");
-        }
+﻿namespace hello_world;
+
+public class hello_world {
+    
+    static void Main(string[] args) {
+        System.Console.WriteLine("hello, world!");
     }
 }
