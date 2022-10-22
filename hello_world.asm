7a09726a8bc0801ffcb171eb48e1dd51a8a1ca76
Alexander Haugerud <84683233+duggurd@users.noreply.github.com>
2022-10-22 12:23:19 +0200

    Update hello_world.asm

--- hello_world.asm
+++ hello_world.asm
@@ -3,22 +3,16 @@ section .text
     global _start
 
 _start:
+        mov     edx,len   
+        mov     ecx,msg   
+        mov     ebx,1     
+        mov     eax,4     
+        int     0x80	  
 
-        ; write our string to stdout.
-
-        mov     edx,len   ; third argument: message length.
-        mov     ecx,msg   ; second argument: pointer to message to write.
-        mov     ebx,1     ; first argument: file handle (stdout).
-        mov     eax,4     ; system call number (sys_write).
-        int     0x80	  ; call kernel.
-
-        ; and exit.
-
-	mov	ebx,0	  ; first syscall argument: exit code.
-        mov     eax,1     ; system call number (sys_exit).
-        int     0x80	  ; call kernel.
+	mov	ebx,0	  
+        mov     eax,1     
+        int     0x80	 
 
 section .data
-
-msg     db      "Hello, world!",0xa      ; the string to print.
-len     equ     $ - msg                  ; length of the string.
+msg     db      "Hello, world!",0xa      
+len     equ     $ - msg                  
